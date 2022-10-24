0953b102b469e4fff6f8c14dc27783a7e54028bb
Schuai40hrs <502153122@qq.com>
2022-10-24 16:54:47 +0800

    modify main.c

--- task1/main.c
+++ task1/main.c
@@ -2,7 +2,29 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	double a;
+	scanf("%lf",&a);
+	double x=0,y;	//y(0)=a
+	y=a;
+	//if输入e^n --> y=exp(a)
+	//if输入pi --> double pi=acos(-1.0); y=pi;
+	double h=0.001;	//定义步长 
+	int i,k;
+	double y_a,y_b;	 
+	printf("x	y\n");
+	for(i=1,k=10;i<k;i++)
+	{
+		x+=h;
+		y_a=y;
+		y*=(1-h);
+		y_b=y;
+		printf("%.3f	%f\n",x,y);
+		if(y_a-y_b>0.001*h)	//当两个y的差小于0.001倍的步长即已收敛 
+			k++;
+		else
+			break;
+	}
+	
 	return 0;
 }
 
